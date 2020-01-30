2638399535cc7207a908e0d275e15ac47d7e4925
Margo Flewelling <flewelling.margo@gmail.com>
2020-01-30 13:58:40 -0700

    Calculate standard deviation

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,32 @@
+class Standard_deviation
+end
+
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+sum = 0
+ages.each do |age|
+sum += age
+end
+puts sum
 
-# When you find the standard deviation, print it out
+mean = sum / ages.length
+puts "mean = #{mean}"
+
+square_differences = []
+ages.each do |age|
+square_differences << ((age - mean) * (age - mean))
+end
+
+puts square_differences
+
+sum_of_square_differences = 0
+square_differences.each do |number|
+sum_of_square_differences += number
+end
+puts "sum of square differences is #{sum_of_square_differences}"
+
+mean_of_squares = sum_of_square_differences / square_differences.length
+puts "mean of squares is #{mean_of_squares}"
+
+
+puts "Standard deviation = #{Math.sqrt(mean_of_squares)}"
